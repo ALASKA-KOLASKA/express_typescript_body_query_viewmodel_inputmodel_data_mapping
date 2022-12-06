5b3fc8976f8d8a905d42187dbec44036224d53d7
ALASKA_KOLASKA <thefreshchannel@mail.ru>
2022-12-06 18:10:08 +0300

    add HTTP STATUSES

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,15 @@ import express from 'express'
 const app = express()
 const port = 3000
 
+const HTTP_STATUSES = {
+    OK_200: 200,
+    CREATED_201: 201,
+    NO_CONTENT_204: 204,
+
+    BAD_REQUEST_400: 400,
+    NOT_FOUND_404: 404,
+}
+
 const jsonBodyMiddleware = express.json()
 app.use(jsonBodyMiddleware)
 
@@ -26,7 +35,7 @@ app.get('/courses/:id', (req, res) => {
     const foundCourse = db.courses.find(c => c.id === +req.params.id)
 
     if (!foundCourse) {
-        res.sendStatus(404);
+        res.sendStatus(HTTP_STATUSES.NOT_FOUND_404);
         return;
     }
 
@@ -35,7 +44,7 @@ app.get('/courses/:id', (req, res) => {
 app.post('/courses', (req, res) =>{
 
     if (!req.body.title){
-        res.sendStatus(400)
+        res.sendStatus(HTTP_STATUSES.BAD_REQUEST_400)
         return;
     }
 
@@ -46,29 +55,29 @@ app.post('/courses', (req, res) =>{
     db.courses.push(createdCourse)
     console.log(createdCourse)
     res
-        .status(201)
+        .status(HTTP_STATUSES.CREATED_201)
         .json(createdCourse)
 })
 app.delete('/courses/:id', (req, res) => {
     db.courses = db.courses.filter(c => c.id !== +req.params.id)
 
-    res.sendStatus(204)
+    res.sendStatus(HTTP_STATUSES.NO_CONTENT_204)
 })
 app.put('/courses/:id', (req, res) => {
     if (!req.body.title) {
-        res.sendStatus(400);
+        res.sendStatus(HTTP_STATUSES.BAD_REQUEST_400);
         return;
     }
 
     const foundCourse = db.courses.find(c => c.id === +req.params.id)
     if (!foundCourse) {
-        res.sendStatus(404);
+        res.sendStatus(HTTP_STATUSES.NOT_FOUND_404);
         return;
     }
 
     foundCourse.title = req.body.title;
 
-    res.sendStatus(204)
+    res.sendStatus(HTTP_STATUSES.NO_CONTENT_204)
 })
 
 
